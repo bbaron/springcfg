af3cd9577a01d721ec2d50f57400e0ca7365f24b
Bruce Baron <baron.br@gmail.com>
2012-03-21 16:30:12 -0400

    removed webapp stuff from springcfg module

--- springcfg/src/main/java/cfg/model/AppConfigRepository.java
+++
@@ -1,7 +0,0 @@
-package cfg.model;
-
-import org.springframework.roo.addon.layers.repository.jpa.RooJpaRepository;
-
-@RooJpaRepository(domainType = AppConfig.class)
-public interface AppConfigRepository {
-}
--- springcfg/src/test/java/cfg/model/AppConfigDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package cfg.model;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = AppConfig.class)
-public class AppConfigDataOnDemand {
-}
