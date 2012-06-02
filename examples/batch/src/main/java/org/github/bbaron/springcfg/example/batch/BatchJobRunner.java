701785d8621f1d080513766e8587fb6b6cbc07ef
Bruce Baron <baron.br@gmail.com>
2012-06-02 16:00:01 -0400

    removed jdk 7 refs

--- examples/batch/src/main/java/org/github/bbaron/springcfg/example/batch/BatchJobRunner.java
+++ examples/batch/src/main/java/org/github/bbaron/springcfg/example/batch/BatchJobRunner.java
@@ -1,28 +1,28 @@
 package org.github.bbaron.springcfg.example.batch;
 
-import static java.nio.file.Files.*;
-import static org.github.bbaron.springcfg.config.Springcfg.*;
-import static org.springframework.core.env.AbstractEnvironment.*;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.springframework.batch.core.launch.support.CommandLineJobRunner;
+//import static java.nio.file.Files.*;
+//import static org.github.bbaron.springcfg.config.Springcfg.*;
+//import static org.springframework.core.env.AbstractEnvironment.*;
+//
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//
+//import org.springframework.batch.core.launch.support.CommandLineJobRunner;
 
 public class BatchJobRunner {
 
 	public static void main(String[] ignored) throws Exception {
-		Path propertiesPath = Paths.get("src", "main", "resources", "batch.properties");
-		Path appcfgDir = Paths.get("target", "appcfg");
-		if (Files.notExists(appcfgDir)) {
-			appcfgDir = createDirectory(appcfgDir);
-			Files.copy(propertiesPath, appcfgDir.resolve(propertiesPath.getFileName()));
-		}
-		System.out.println("cfg dir = " + appcfgDir);
-		System.setProperty(ACTIVE_PROFILES_PROPERTY_NAME, "deployed");
-		System.setProperty(APP_CFG_PROPERTY_NAME, appcfgDir.toString());
-		String[] args = { "classpath:/launch-context.xml", "job1" };
-		CommandLineJobRunner.main(args);
+//		Path propertiesPath = Paths.get("src", "main", "resources", "batch.properties");
+//		Path appcfgDir = Paths.get("target", "appcfg");
+//		if (Files.notExists(appcfgDir)) {
+//			appcfgDir = createDirectory(appcfgDir);
+//			Files.copy(propertiesPath, appcfgDir.resolve(propertiesPath.getFileName()));
+//		}
+//		System.out.println("cfg dir = " + appcfgDir);
+//		System.setProperty(ACTIVE_PROFILES_PROPERTY_NAME, "deployed");
+//		System.setProperty(APP_CFG_PROPERTY_NAME, appcfgDir.toString());
+//		String[] args = { "classpath:/launch-context.xml", "job1" };
+//		CommandLineJobRunner.main(args);
 	}
 }
